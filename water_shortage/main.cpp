41ed94ec76f16828b7b5793ecd6244ab0edde411
Adele Hurabielle <adele.hurabielle-claverie@telecom-paristech.fr>
2014-05-25 22:58:50 +0200

    Water shortage : first (false) version

---
+++ water_shortage/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+static void water_level(void) {
+  vector<pair<float, float>> citerns;
+  float min_level, height, width, depth;
+  float volume_left, level, sub_level;
+  int citerns_nb;
+  cin >> citerns_nb;
+  while (citerns_nb--) {
+    cin >> min_level >> height >> width >> depth;
+    citerns.push_back(make_pair(min_level, width*depth));
+    citerns.push_back(make_pair(min_level+height, -width*depth));
+  }
+  cin >> volume_left;
+  sort(citerns.begin(), citerns.end());
+  level = 0;
+  for (pair<float, float> item: citerns) {
+    if (item.second == 0) {
+      level = item.first;
+      continue;
+    }
+    sub_level = volume_left / item.second;
+    if (sub_level < (item.first - level)) {
+      cout << level + sub_level;
+      return;
+    }
+    else {
+      volume_left -= item.second * (item.first - level);
+      level = item.first;
+    }
+  }
+  cout << "OVERFLOW" << endl;
+  return;
+}
+
+int main() {
+  int cases_nb;
+  cin >> cases_nb;
+  while (cases_nb--) {
+    water_level();
+    if (cases_nb)
+      cout << endl;
+  }
+  return 0;
+}
